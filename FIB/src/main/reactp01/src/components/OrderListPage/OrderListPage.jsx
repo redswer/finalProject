149a8950b8257392607abc1ee0d55c610d76f4ba
DESKTOP-Q2O1JV3\user <starlight1532@gmail.com>
2023-12-17 22:37:13 +0900

    orderListPage만 손밧다

--- FIB/src/main/reactp01/src/components/OrderListPage/OrderListPage.jsx
+++ FIB/src/main/reactp01/src/components/OrderListPage/OrderListPage.jsx
@@ -7,51 +7,61 @@ import axios from 'axios';
 
 function OrderListPage() {
 
-    const loginID = sessionStorage.getItem("loginID");
-    const [detailData , setDetailData] = useState([]);
+  const loginID = sessionStorage.getItem("loginID");
+  const [detailData, setDetailData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(`/mpdetail/mpdList?id=${loginID}`)
+      .then((res) => {
+        setDetailData(res.data);
+        console.log(`주문상세 성공 : `, res.data);
+        // alert(`주문상세 성공 `);
+      }).catch((e) => {
+        console.log(`주문상세 실패 : `, e.messege);
+        alert(`주문상세 실패`);
+      })
+  }, []);
 
+  return (
+    <table className="orderListPageContainer">
+      <thead>
+        <tr>
+          <th style={{ width: '15%' }}>주문일자 / 주문번호</th>
+          <th style={{ width: '45%' }}>상품정보</th>
+          <th style={{ width: '10%' }}>가격&수량</th>
+          <th style={{ width: '15%' }}>배송상태</th>
+          <th style={{ width: '15%' }}>주문 취소</th>
+        </tr>
+      </thead>
+      <tbody>
+        {detailData.map((d, i) => (
 
-    // useEffect(()=> {
-    //     axios
-    //         .get(`/memberdetailpayment/mpdList?id=${loginID}`)
-    //         .then((res)=> {
-    //             setDetailData(res.data);
-    //             console.log(`주문상세 성공 : `, res.data);
-    //             alert(`주문상세 성공 `);
-    //         }).catch((e)=>{
-    //             console.log(`주문상세 실패 : `, e.messege);
-    //             alert(`주문상세 실패`);
-    //         })
-    // },[]);
+          <tr key={i} className='orderDetailItem'>
+            <td className='orderDetailDataPmCode'>
+              <div className='orderDetailData'>{d.payment_date}</div>
+              <div className='orderDetailPmCode'>{`[ ${d.member_payment_code} ]`}</div>
+            </td>
 
-    return (
-        <table className="orderListPageContainer">
-          <thead>
-            <tr>
-              <th style={{ width: '15%' }}>주문일자&주문번호</th>
-              <th style={{ width: '45%' }}>상품정보</th>
-              <th style={{ width: '10%' }}>가격&수량</th>
-              <th style={{ width: '15%' }}>배송상태</th>
-              <th style={{ width: '15%' }}>주문 취소</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* {detailData.map((d,i)=> (
+            <td className='orderDetailItemInfomation'>
+              <img src={`../img/yeonsu.jpg`} className='orderListItemImg'></img>
+              <div>
+                <span>{d.title}제목</span>
+                <span>{d.domestic}</span>
+                <span>{d.protype}</span>
+                <div></div>
+                <div></div>
+              </div>
+            </td>
 
-                <tr key={i}>
-                    <td> & {d.member_payment_code}</td>
-                    <td>{`${d.protype} ${d.domestic} ${d.title} ${d.image}`}</td>
-                    <td>{`${d.proamount} ${d.price}`}</td>
-                    <td>상품준비중</td>
-                    <td><button >주문 취소</button></td>
-                </tr>
-            ))} */}
-
-            오더리스트
-          </tbody>
-        </table>
-    );
+            <td>{`${d.proamount} ${d.price}`}</td>
+            <td>상품준비중</td>
+            <td><button >주문 취소</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default OrderListPage;
