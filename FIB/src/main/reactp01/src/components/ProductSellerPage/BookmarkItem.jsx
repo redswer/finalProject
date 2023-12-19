d877a1544c50cbd09ebe0a52241f615b607fecbd
DESKTOP-Q2O1JV3\user <starlight1532@gmail.com>
2023-12-19 09:53:53 +0900

    no message

--- FIB/src/main/reactp01/src/components/ProductSellerPage/BookmarkItem.jsx
+++ FIB/src/main/reactp01/src/components/ProductSellerPage/BookmarkItem.jsx
@@ -31,7 +31,7 @@ const BookmarkItem = (props) => {
         console.log(`장바구니 담았어요 :`, response);
         console.log(`response.OK :`, response.status);
         console.log('========================================');
-        // alert(`장바구니 담았어요 : ${response.data}`);
+        alert(`장바구니 담았어요 : ${response.data}`);
 
       }).catch((err) => {
         alert(`담기 실패!! ${err.message}`);
@@ -42,7 +42,7 @@ const BookmarkItem = (props) => {
       .then((response) => {
         console.log(`장바구니 담기  : `, response.data);
         console.log('========================================');
-        alert(`장바구니 담기 성공 `, response.data);
+        alert(`북마크지우기 `, response.data);
 
       }).catch((err) => {
         alert(`카트목록 삭제 실패!! ${err.message}`);
@@ -127,12 +127,12 @@ const BookmarkItem = (props) => {
           <img src={`../img/yeonsu.jpg`} alt="" className='bookmarkItemImage' />
         </div>
         <div className='bookmarkItemCartDeleteDiv'>
-          <div className='bookmarkItemCart'>
+          <div className='bookmarkItemCart' onClick={saveOnCart}>
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
           </div>
-          <div className='bookmarkItemDelete'>
+          <div className='bookmarkItemDelete' onClick={DeleteThisProduct}>
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
               <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
             </svg>
